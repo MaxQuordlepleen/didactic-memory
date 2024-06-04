5f6b443ce044ef5c80bb3456c47b55d6cebbb13c
MaxQuordlepleen <53817552+MaxQuordlepleen@users.noreply.github.com>
2024-06-04 15:29:17 +0100

    Add files via upload

---
+++ install-aws-tools.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#sudo apt install awscli
+
+#git clone https://github.com/nccgroup/ScoutSuite \
+#&& cd ScoutSuite \
+#&& virtualenv -p python3 venv \
+#&& source venv/bin/activate \
+#&& pip install -r requirements.txt \
+#rem python scout.py --help
+#&& cd .. \
+
+#!!!not working
+#git clone https://github.com/duo-labs/cloudmapper
+#sudo apt-get install python3-dev autoconf automake libtool python3-tk jq build-essential
+#cd cloudmapper/
+#virtualenv -p python3 venv && source venv/bin/activate
+#pip install -r requirements.txt
+#cd ..
+
+#git clone https://github.com/nccgroup/PMapper \
+#&& cd PMapper \
+#!!!Monkey Patch
+#&& sed -i 's/from collections import Mapping, MutableMapping, OrderedDict/from collections\.abc import Mapping, MutableMapping\nfrom collections import OrderedDict/' principalmapper/util/case_insensitive_dict.py \
+#&& pip install . \
+#cd ..
+
+#mkdir aws_escalate \
+#&& curl -o aws_escalate/aws_escalate.py https://raw.githubusercontent.com/RhinoSecurityLabs/Security-Research/master/tools/aws-pentest-tools/aws_escalate.py \
+#&& pip install boto3 \
+#&& chmod 700 aws_escalate/aws_escalate.py
+
+#sudo /bin/sh -c "$(curl -fsSL https://steampipe.io/install/steampipe.sh)" \
+#&& steampipe plugin install aws
+
+#sudo /bin/sh -c "$(curl -fsSL https://powerpipe.io/install/powerpipe.sh)" \
+#&& mkdir pp-dashboards && cd pp-dashboards \
+#&& powerpipe mod init \
+#&& powerpipe mod install github.com/turbot/steampipe-mod-aws-compliance \
+
+#pip install prowler
+
+#pip3 install -U pacu
+#echo "May have been a pip error there installing pacu but it seems to work ok"
